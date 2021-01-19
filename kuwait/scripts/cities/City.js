cb5025e501e2d29c5649604365fef96aadf245b6
Ccourter11 <courtercalvin@yahoo.com>
2021-01-19 13:46:11 -0600

    put new data into citys and city data provider files

---
+++ kuwait/scripts/cities/City.js
@@ -0,0 +1,11 @@
+export const City = (city) => {
+  return;
+  `<div class="cityCard">
+        <p class="cityCard__name">${city.name}</p>
+        <p class="cityCard__population">${city.population}</p>
+        <p class="cityCard__description">${city.description}</p>
+        <img src="${city.image}" alt="city image" class="cityCard__img"></img>
+    </div>
+    
+            `;
+};
--- kuwait/scripts/cities/CityDataProvider.js
+++ kuwait/scripts/cities/CityDataProvider.js
@@ -1,15 +1,15 @@
 export const cityCollection = [
     {
-        name: "city one",
-        population: ,
-        description: "",
+        name: "Kuwait City",
+        population: 4,000,000,
+        description: "This global city has a metro population estimated at 4.1 million in 2016. Nearly 98% of the population in Kuwait is urbanized, and about 83% of the total population resides in the capital, Kuwait City.",
         image: "",
     },
     {
-        name:"city two",
-        population: ,
-        description: "",
+        name:"Al Jahra",
+        population: 67,311,
+        description: "Al Jahra is a town located 32 kilometres west of Kuwait City in Kuwait. Al Jahra is the capital of the Al Jahra Governorate of Kuwait as well as the surrounding Al Jahra District which is agriculturally based.",
         image: "",
-    }
+    },
 
-]
+];
