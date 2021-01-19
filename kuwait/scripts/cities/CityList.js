4b5d52501d4b2153dd6fbfdf10da23e9a30b8afe
Ccourter11 <courtercalvin@yahoo.com>
2021-01-19 14:19:19 -0600

    new city information

--- kuwait/scripts/cities/CityList.js
+++ kuwait/scripts/cities/CityList.js
@@ -1,4 +1,7 @@
-// import { City } from "./City.js";
+import { City } from "./City.js";
+import { largeCity } from "./CityDataProvider.js";
+
+// const buildCityListHTML = ()
 
 // export const CityList = () => {
 //     const contentElement = document.querySelector(".cityCard");
